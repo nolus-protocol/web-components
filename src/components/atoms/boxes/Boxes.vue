f19418551127fba1b71c694e369aba2f6b286afe
Teodor Dobrev <teodor.working@gmail.com>
2024-03-07 12:00:16 +0200

    add boxes component

---
+++ src/components/atoms/boxes/Boxes.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="button button-secondary rounded-lg px-6 py-4 font-medium text-14 flex gap-3"
+          type="button" @click="onClick">
+    <span v-if="icon" :class="[icon]" class="icon text-[32px]" />
+    {{ label }}
+  </button>
+</template>
+
+<script lang="ts" setup>
+export interface WalletProps {
+  label?: string;
+  icon?: string;
+}
+
+const props = withDefaults(defineProps<WalletProps>(), {
+  label: 'Connect Wallet'
+
+});
+
+const emit = defineEmits<{
+  (e: 'click', id: number): void;
+}>()
+
+const onClick = () => {
+  emit('click', 1)
+}
+
+</script>
