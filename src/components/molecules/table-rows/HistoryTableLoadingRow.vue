826cadfd4744ab6b7458456c9679695a52902e67
Alexander Makedonski <alexanderm@prime-code.com>
2024-06-25 10:53:59 +0300

    fix(history table loading button)

--- src/components/molecules/table-rows/HistoryTableLoadingRow.vue
+++ src/components/molecules/table-rows/HistoryTableLoadingRow.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="['flex flex-col gap-1 border-b-[1px] border-border-color py-3 md:flex-row md:gap-0', $attrs.class]">
-    <div :class="['flex flex-1 items-center gap-0.5 text-12 font-medium text-neutral-400', $attrs.class]">
+    <div :class="['flex flex-1 items-center gap-0.5 text-14 font-medium uppercase text-neutral-400', $attrs.class]">
       <a
         class="flex items-center gap-1 text-primary-50"
         target="_blank"
@@ -9,7 +9,7 @@
         {{ props.status }}
       </a>
     </div>
-    <div :class="['flex flex-1 items-center gap-0.5 text-12 font-medium text-neutral-400', $attrs.class]">
+    <div :class="['flex flex-1 items-center gap-0.5 text-12 text-14 font-medium text-neutral-400', $attrs.class]">
       <span :class="['text-neutral-typography-200']">{{ props.action }}</span>
     </div>
     <div
