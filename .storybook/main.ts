23e230d24a59ec9a1ff0f46cd6be99f8f1a7fd54
boyankostov <mail@boyankostov.com>
2026-02-13 15:36:30 +0200

    Add storybook accessibility plugin

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,11 @@ import type { StorybookConfig } from "@storybook/vue3-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-themes"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-themes",
+    "@storybook/addon-a11y"
+  ],
   framework: "@storybook/vue3-vite"
 };
 export default config;
